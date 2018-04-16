2bb2191295b8029ed75f097f014d609f129f45b4
tyetao <765068439@qq.com>
2018-04-16 20:20:28 +0800

    '环境搭建'

---
+++ Hello.java
@@ -0,0 +1,11 @@
+public class Hello {
+  public static void main(String[] args) {
+    System.out.println("Hello Word");
+  }
+}
+
+class ABC {
+  public static void main(String[] args) {
+    System.out.println("ABC");
+  }
+}
---
+++ 设置java开发环境变量.bat
@@ -0,0 +1,3 @@
+set JAVA_HOME=C:\Program Files\Java\jdk-9.0.1
+set path=%JAVA_HOME%\bin;%path%
+set CLASSPATH=.;%JAVA_HOME%\lib\dt.jar;%JAVA_HOME%\lib\tools.jar
